053ab8958d7226b90000c956ba4a0c2ea919cb1b
思尘 <ward_1992@msn.cn>
2020-05-19 16:42:06 +0800

    update casebook list end

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,21 @@ const app = getApp()
 
 Page({
   data: {
+    caseBookList: []
   },
   onLoad: function () {
+    const caseBookList = []
+    for (let i = 0; i < 21; i++) {
+      caseBookList.push({
+        id: i + 1,
+        url: `http://oss-materials.ifable.cn/conan/m${i + 1}.jpg?imageView2/0/interlace/1`,
+        logo: `http://oss-materials.ifable.cn/conan/m${i + 1}logo.png`,
+        year: 1997 + i,
+        waiting: 1997 + i > 2009
+      })
+    }
+    this.setData({
+      caseBookList: caseBookList
+    })
   },
 })
